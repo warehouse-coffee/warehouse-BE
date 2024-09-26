b0a65e557b1fdb0090be7e835cb390e8fad3281a
Cevan <110558291+Cevan23@users.noreply.github.com>
2024-09-27 00:22:15 +0700

    add base

---
+++ src/Application/CompanyOwner/Queries/GetCompanyOwnerDetail/GetCompanyOwnerDetailQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warehouse_BE.Application.CompanyOwner.Queries.GetCompanyOwnerDetail
+{
+    internal class GetCompanyOwnerDetailQuery
+    {
+    }
+}
